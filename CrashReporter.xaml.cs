0acc94f44699fe5d63e14bcfb21f5b8ec17048d0
Pursuit <fr.pursuit@outlook.com>
2016-07-20 14:41:22 +0200

    Crash report improved

--- CrashReporter.xaml.cs
+++ CrashReporter.xaml.cs
@@ -3,6 +3,7 @@ using PursuitLib.Wpf;
 using System;
 using System.Diagnostics;
 using System.IO;
+using System.Text;
 using System.Windows;
 
 namespace GTAVModdingLauncher
@@ -39,9 +40,7 @@ namespace GTAVModdingLauncher
 				Log.Warn("Unable to create crash report interface.");
 			}
 
-			string message = "GTA V Modding Launcher " + Launcher.Version + " crash report.\n";
-			message += "Generated on " + String.Format("{0:dd/MM/yyyy HH:mm:ss}", DateTime.Now) + "\n\n\n";
-			message += this.exception;
+			string message = this.FillCrashReport();
 
 			this.Report.Text = message;
 
@@ -57,6 +56,72 @@ namespace GTAVModdingLauncher
 			{
 				writer.Write(message);
 			}
+		}
+
+		private string FillCrashReport()
+		{
+			StringBuilder report = new StringBuilder();
+			report.Append("---- GTA V Modding Launcher " + Launcher.Version + " crash report ----\n");
+			report.Append("Generated on " + String.Format("{0:dd/MM/yyyy HH:mm:ss}", DateTime.Now) + "\n\n\n");
+
+			report.Append("-- Thrown exception --\n");
+			report.Append(this.exception.ToString() + "\n\n");
+
+			report.Append("-- Launcher state --\n");
+			try
+			{
+				report.Append("Is initialized: " + (Launcher.Instance != null) + '\n');
+				report.Append("Is window initialized: " + (Launcher.Instance.Window != null) + '\n');
+				if(Launcher.Instance.Profiles != null)
+				{
+					report.Append("Current profile index: " + Launcher.Instance.Profiles.CurrentProfile + '\n');
+					report.Append("Current profile: ");
+					try
+					{
+						report.Append(Launcher.Instance.Profiles[Launcher.Instance.Profiles.CurrentProfile]);
+					}
+					catch(Exception)
+					{
+						report.Append("~Unexpected error~");
+					}
+					report.Append('\n');
+				}
+			}
+			catch(Exception)
+			{
+				report.Append("~Unexpected error~\n");
+			}
+
+			report.Append('\n');
+
+			report.Append("-- UI state --\n");
+			try
+			{
+				if(Launcher.Instance != null && Launcher.Instance.UiManager != null)
+				{
+					report.Append("Selected profile index: " + Launcher.Instance.UiManager.SelectedProfile + '\n');
+					report.Append("Selected profile: ");
+					try
+					{
+						report.Append(Launcher.Instance.Profiles[Launcher.Instance.UiManager.SelectedProfile]);
+					}
+					catch(Exception)
+					{
+						report.Append("~Unexpected error~");
+					}
+					report.Append('\n');
+					report.Append("Is working: " + Launcher.Instance.UiManager.Working + '\n');
+					report.Append("Are buttons enabled: " + Launcher.Instance.UiManager.ButtonsEnabled + '\n');
+				}
+				else report.Append("Unable to get UI state");
+			}
+			catch(Exception)
+			{
+				report.Append("~Unexpected error~\n");
+			}
+
+
+			return report.ToString();
 		}
 
 		private void Close(object sender, EventArgs e)
